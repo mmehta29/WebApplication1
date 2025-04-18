b93d6e14c0f908148b05faf0ac9fd9ee3c910335
mmehta29 <mmehta29@EN4228126-214>
2025-04-18 15:42:04 -0700

    Add project files.

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary1;
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string hashedValue = Class1.ComputeSha256Hash("MySecretPassword123");
+            System.Diagnostics.Debug.WriteLine("Hashed Password: " + hashedValue);
+        }
+    }
+}
---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication1
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Application has started successfully at: " + DateTime.Now);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            System.Diagnostics.Debug.WriteLine("Unhandled Application Error: " + ex.Message);
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Application has ended successfully at: " + DateTime.Now);
+        }
+    }
+}
