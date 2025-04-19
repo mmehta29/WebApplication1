66117d41444a3c89c72a0b0c7ed084822d1ef307
cnmoore4 <115666195+cnmoore4@users.noreply.github.com>
2025-04-19 11:08:31 -0700

    Update CreateAccount.aspx.cs
    added comments

--- WebApplication1/CreateAccount.aspx.cs
+++ WebApplication1/CreateAccount.aspx.cs
@@ -1,3 +1,4 @@
+// Page created by: Carissa Moore (1224352909)
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using System.IO;
 
 namespace WebApplication1
 {
-    public partial class CreateAccount : System.Web.UI.Page
+        public partial class CreateAccount : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,36 +19,39 @@ namespace WebApplication1
 
         protected void createAccountBtn_Click(object sender, EventArgs e)
         {
+            // getting entered information
             string username = usernameTb.Text;
             string password = passwordTb.Text;
             string repass = repassTb.Text;
+            // if the two passwords don't match, the user is notified
             if (repass != password)
             {
                 resultLbl.Text = "Passwords do not match!";
             } else
             {
-                bool result = addUser(username, password);
-                if (result)
+                bool result = addUser(username, password);  // adds the user to Users.xml if they do not exist
+                if (result)                                 // if the user was successfully added
                 {
-                    HttpCookie userCookie = new HttpCookie("UserLogin");
+                    HttpCookie userCookie = new HttpCookie("UserLogin");    // creates a new cookie with the username and password
                     userCookie["UserID"] = username;
                     userCookie["Password"] = password;  // add hash?
 
-                    Response.Cookies.Add(userCookie);
-                    Response.Redirect("Login.aspx");
+                    Response.Cookies.Add(userCookie);           
+                    Response.Redirect("Login.aspx");                       // takes the user to the login page
                 }
             }
         }
-
+        
+        // method to add the new user to Users.xml
         bool addUser(string username, string password)
         {
-            string path = Server.MapPath("~/App_Data/Users.xml");
+            string path = Server.MapPath("~/App_Data/Users.xml");   // path to Users.xml
             XmlDocument doc = new XmlDocument();
             
-            if (File.Exists(path))
+            if (File.Exists(path))      // if Users.xml exists, it is opened
             {
                 doc.Load(path);
-            } else
+            } else                     // if Users.xml was deleted, create a new one
             {
                 XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
                 XmlElement root = doc.CreateElement("Users");
@@ -66,24 +70,24 @@ namespace WebApplication1
                 doc.Save(path);
             }
 
-            if (doc.SelectSingleNode($"/Users/User[Username='{username}']") != null)
+            if (doc.SelectSingleNode($"/Users/User[Username='{username}']") != null)    // if the username already exists, the user must pick a new one
             {
                 resultLbl.Text = "Username already exists";
                 return false;
             }
 
-            XmlElement userElement = doc.CreateElement("User");
+            XmlElement userElement = doc.CreateElement("User");     // User element for xml
 
-            XmlElement userNode = doc.CreateElement("Username");
+            XmlElement userNode = doc.CreateElement("Username");    // username node with given username
             userNode.InnerText = username;
 
-            XmlElement passNode = doc.CreateElement("Password");
+            XmlElement passNode = doc.CreateElement("Password");    // password node with given password
             passNode.InnerText = password;
 
-            userElement.AppendChild(userNode);
+            userElement.AppendChild(userNode);                      // adding the usernname and password to the User element
             userElement.AppendChild(passNode);
 
-            doc.DocumentElement.AppendChild(userElement);
+            doc.DocumentElement.AppendChild(userElement);           // adding the new User to Users.xml
             doc.Save(path);
 
             return true;
