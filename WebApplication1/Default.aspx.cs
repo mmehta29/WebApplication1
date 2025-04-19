3c3a9e6b5cb7220d1153ecd2924ae30dd3e938b0
cnmoore4 <115666195+cnmoore4@users.noreply.github.com>
2025-04-18 18:54:47 -0700

    Update Default.aspx.cs

--- WebApplication1/Default.aspx.cs
+++ WebApplication1/Default.aspx.cs
@@ -15,5 +15,57 @@ namespace WebApplication1
             string hashedValue = Class1.ComputeSha256Hash("MySecretPassword123");
             System.Diagnostics.Debug.WriteLine("Hashed Password: " + hashedValue);
         }
+        
+        // member button
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("~/Member/Member.aspx");
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        // staff button
+        protected void StaffBtn_Click(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                if (Session["Staff"] == null || (bool)Session["Staff"] == false)
+                {
+                    result1Lbl.Text = "You do not have access to this page.";
+                } else
+                {
+                    Response.Redirect("~/Staff/Staff.aspx");
+                }
+            } else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void createAccBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CreateAccount.aspx");
+        }
+
+        protected void logoutBtn_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+
+            Response.Cookies.Clear();
+
+            FormsAuthentication.SignOut();
+            resultLbl.Text = "Successfully signed out!";
+        }
+
+        protected void loginBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
     }
 }
