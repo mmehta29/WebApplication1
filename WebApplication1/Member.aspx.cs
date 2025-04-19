74b909726e4e8b6f33820f6dacc541089d6d2812
cnmoore4 <115666195+cnmoore4@users.noreply.github.com>
2025-04-19 11:17:37 -0700

    Update Member.aspx.cs
    added comments

--- WebApplication1/Member.aspx.cs
+++ WebApplication1/Member.aspx.cs
@@ -1,3 +1,4 @@
+// Page created by: Carissa Moore (1224352909)
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,21 +8,28 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication1
 {
-    public partial class Member : System.Web.UI.Page
+       public partial class Member : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if ((bool)Session["Staff"] == true)
+            if ((bool)Session["Staff"] == true)     // if user is Staff they can access the page
             {
 
             } else
             {
+                // if the User is not a member, they are redirected back to the default page
                 if ((bool)Session["Member"] == false || Session["Member"] == null)
                 {
                     Response.Redirect("~/Default.aspx");
                 }
             }
            
+        }
+
+        // button to access the Weather Service
+        protected void weatherServiceBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/WeatherService.aspx");
         }
     }
 }
