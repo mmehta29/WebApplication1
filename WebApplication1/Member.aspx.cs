d41c15aaee36feda67b35a733129540104233ce3
cnmoore4 <115666195+cnmoore4@users.noreply.github.com>
2025-04-26 11:03:04 -0700

    Update Member.aspx.cs
    updated for assignment 6

--- WebApplication1/Member.aspx.cs
+++ WebApplication1/Member.aspx.cs
@@ -1,4 +1,5 @@
-﻿// Page created by: Carissa Moore (1224352909)
+// Page created by: Carissa Moore (1224352909)
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,15 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication1
 {
-       public partial class Member : System.Web.UI.Page
+    public partial class Member : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
             if ((bool)Session["Staff"] == true)     // if user is Staff they can access the page
             {
 
-            } else
+            }
+            else
             {
                 // if the User is not a member, they are redirected back to the default page
                 if ((bool)Session["Member"] == false || Session["Member"] == null)
@@ -23,13 +25,30 @@ namespace WebApplication1
                     Response.Redirect("~/Default.aspx");
                 }
             }
-           
+
+            // sets a Welcome textbox with the user's username
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                welcomeLbl.Text = "Welcome, " + Context.User.Identity.Name + "!";
+            }
         }
 
         // button to access the Weather Service
         protected void weatherServiceBtn_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/WeatherService.aspx");
+        }
+
+        // button to test the Encryption Service
+        protected void btnTryEncrypt_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.ServiceClient client = new ServiceReference1.ServiceClient();
+            string inputText = txtEncryptInput.Text;        // getting user input
+            string encrypted = client.Encrypt(inputText);       // encrypting user input
+
+            lblEncryptResult.Text = "Encrypted Output: " + encrypted;   // displaying the encrypted string to the user
+
+            client.Close();
         }
     }
 }
